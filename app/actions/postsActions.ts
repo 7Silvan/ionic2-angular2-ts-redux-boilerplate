8fb548fa7a79b67eb8b3e7037822475edddae10b
Mark Ericksen <brainlid@gmail.com>
2016-06-25 14:13:17 -0600

    fixed all typescript errors, reworked state to be immutable objects
    - removed redux-immutable (out of date and reworked to not need it)
    - cleans up page usage of state
    - makes state more explicitly typed
    - fixed accidental bad action constant values
    working on new typescript-friendly way of managing state

--- app/actions/postsActions.ts
+++ app/actions/postsActions.ts
@@ -1,13 +1,13 @@
 import * as actionTypes from '../actionTypes.ts';
-import {fromJS} from 'immutable';
 import jsonRequest from '../utils/jsonRequest';
+import {PostState} from '../stores/initial_state';
 import {Post} from '../post';
 
 // Success.
 const setSuccessState = (response) => {
   return {
-    data: fromJS({
-      items: response.data.children.map((p)=> Post.fromJS(p.data)),
+    data: new PostState({
+      items: Post.fromJSArray(response.data.children),
       status: 'success',
     }),
     type: actionTypes.POSTS_FETCH_SUCCESS,
@@ -17,8 +17,8 @@ const setSuccessState = (response) => {
 // Error.
 const setErrorState = (error) => {
   return {
-    data: fromJS({
-      items: [],
+    data: new PostState({
+      items: Post.fromJSArray([]),
       message: error,
       status: 'error',
     }),
@@ -28,8 +28,8 @@ const setErrorState = (error) => {
 
 const setLoadingState = () => {
   return {
-    data: fromJS({
-      items: [],
+    data: new PostState({
+      items: Post.fromJSArray([]),
       status: 'loading',
     }),
     type: actionTypes.POSTS_FETCH_INIT,
