9f486c6331be5e62914d45bededd76f4f2eb7a85
Mark Ericksen <brainlid@gmail.com>
2016-06-25 14:21:24 -0600

    fixed bad copy-paste action constant change

--- app/actionTypes.ts
+++ app/actionTypes.ts
@@ -1,7 +1,7 @@
-export const DUMMY_FETCH_FAIL: string     = 'POSTS_FETCH_SUCCESS';
-export const DUMMY_FETCH_INIT: string     = 'POSTS_FETCH_SUCCESS';
-export const DUMMY_FETCH_SUCCESS: string  = 'POSTS_FETCH_SUCCESS';
+export const DUMMY_FETCH_FAIL: string     = 'DUMMY_FETCH_FAIL';
+export const DUMMY_FETCH_INIT: string     = 'DUMMY_FETCH_INIT';
+export const DUMMY_FETCH_SUCCESS: string  = 'DUMMY_FETCH_SUCCESS';
 
-export const POSTS_FETCH_FAIL: string     = 'POSTS_FETCH_SUCCESS';
-export const POSTS_FETCH_INIT: string     = 'POSTS_FETCH_SUCCESS';
+export const POSTS_FETCH_FAIL: string     = 'POSTS_FETCH_FAIL';
+export const POSTS_FETCH_INIT: string     = 'POSTS_FETCH_INIT';
 export const POSTS_FETCH_SUCCESS: string  = 'POSTS_FETCH_SUCCESS';
